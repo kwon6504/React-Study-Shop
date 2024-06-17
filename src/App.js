bc05e7ec1737c6a58b8929d6a178106a7acb5c3a
DESKTOP-5UVG7U6\kwon <gurdud0328@naver.com>
2024-06-17 14:32:15 +0900

    import, export 문법

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import { Button, Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 // 1.import 작명 from './이미지경로'
 // 2.이미지 경로가 필요한 곳에서 작명한걸 사용
 import bg from './img/bg.png';
+import { useState } from 'react';
 // 리액트는 사이트 발행 전에 html, js, css 파일을 압축함 (bundling)
 // (참고) public 폴더에 있던건 압축안됨
 // <img src={process.env.PUBLIC_URL + '/logo192.png'}></img>
+import data from './data.js';
 
 function App() {
+
+  //import, export 문법
+  //1.변수를 export 하고 = export default 변수명, 변수가 2개라면 export default {변수명1, 변수명2}
+  //2.import하면 끝 = import 작명 from './data.js', 2개를 가져오려면 import {변수명1, 변수명2} from './data.js'
+  //(참고) Component도 export 가능, []=array, {}=object 라서 서로 섞어서 가능하다.
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -25,27 +34,34 @@ function App() {
 
       <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
 
+      {shoes.map(function (a, i) {
+        return (
+          <>
+            <Goods shoes={shoes[i]}></Goods>
+          </>
+        )
+      }
+      )
+      }
+
+    </div>
+  );
+}
+
+function Goods(props) {
+
+  return (
+    <>
       <Container>
         <Row>
           <Col>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='80%'></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='80%'></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='80%'></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <img src={'https://codingapple1.github.io/shop/shoes' + (props.shoes.id + 1) + '.jpg'} width='50%'></img>
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.price} Won</p>
           </Col>
         </Row>
       </Container>
-
-    </div>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,31 @@
+// array 자료
+// let a = ['kim',20];
+// a[0];
+
+// object 자료
+// let b = {name : 'kim', age : 20 };
+// b.name
+// b.age
+
+let data = [
+    {
+        id: 0,
+        title: "White and Black",
+        content: "Born in France",
+        price: 120000
+    },
+    {
+        id: 1,
+        title: "Red Knit",
+        content: "Born in Seoul",
+        price: 110000
+    },
+    {
+        id: 2,
+        title: "Grey Yordan",
+        content: "Born in the States",
+        price: 130000
+    }
+]
+
+export default data;
