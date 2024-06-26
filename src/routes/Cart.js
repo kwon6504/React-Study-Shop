517cb434fa356fc5fd6f1f14ec93237595fd6af5
DESKTOP-5UVG7U6\kwon <gurdud0328@naver.com>
2024-06-26 09:31:40 +0900

    Redux 사용법 기본문법 적용해보기

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,9 +1,13 @@
+/* eslint-disable */ //Lint 끄는 기능
 import { Table } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 function Cart() {
 
     //Redux 사용하면 컴포넌트들이 props 없이 state 공유가능(React 구인시 대부분 Redux 요구)
-    
+    let a = useSelector((state)=>state.user); //Redux store 가져와줌, store에 있던 state 남음
+    let b = useSelector((state)=>state.myShoes);
+
     return (
         <div>
             <Table>
@@ -16,12 +20,19 @@ function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
+                {b.map(function (a, i) {
+                  return (
+                    <tr key={i}>
+                        <td>{b[i].id}</td>
+                        <td>{b[i].name}</td>
+                        <td>{b[i].count}</td>
                         <td>안녕</td>
                     </tr>
+                  )
+                }
+                )
+                }
+                    
                 </tbody>
             </Table>
         </div>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,30 @@
+/* eslint-disable */ //Lint 끄는 기능
 //Redux 사용이유
-//컴포넌트간 state 공유 편해짐
-import { configureStore } from '@reduxjs/toolkit'
+//컴포넌트간 state 공유 편해짐(props전송이 필요없어진다.)
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let user = createSlice({ //(state 하나를 slice라고 부름)
+  name: 'user', //'state이름'
+  initialState: 'kwon' //대충로그인한 유저이름'state값'
+})
+
+let stock = createSlice({ //(state 하나를 slice라고 부름)
+  name: 'stock', //'state이름'
+  initialState: [10, 11, 12] //대충로그인한 유저이름'state값'
+})
+
+let myShoes = createSlice({
+  name: 'myShoes',
+  initialState: [
+    { id: 0, name: 'White and Black', count: 2 },
+    { id: 2, name: 'Grey Yordan', count: 1 }
+  ]
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: { // 작명 : state이름.reducer
+    user: user.reducer,
+    stock: stock.reducer,
+    myShoes : myShoes.reducer
+  }
 }) 
